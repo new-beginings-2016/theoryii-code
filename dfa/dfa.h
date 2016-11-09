cd1d835520e587d66dca4cba3cde6d4288a5b64d
Caleb Tillman <caleb.tillman@gmail.com>
2016-11-09 14:14:55 -0800

    initial commit for a DFA

---
+++ dfa/dfa.h
@@ -0,0 +1,7 @@
+/*
+    New Beginnings, Theory II Code Base
+
+    This file contains code for a deterministic finite automaton states 1,...,N;
+    with an alphabet of {0, 1}; a state transition table; a given start state; and
+    a given set of final states.
+*/
